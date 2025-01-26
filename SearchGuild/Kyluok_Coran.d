448c94eb28b33f993ac2b06526d637ed278a44de
Austin-BG <79753790+Austin-BG@users.noreply.github.com>
2025-01-26 21:17:17 +0300

    Add files via upload

---
+++ SearchGuild/Kyluok_Coran.d
@@ -0,0 +1,26 @@
+APPEND O#CORANJ
+
+IF ~GlobalGT("ANSearchGuild","GLOBAL",22) Global("CoranAN0450","AN0450",2)~ THEN BEGIN CoranAN0450Talk
+  SAY @0
+IF ~~ THEN DO ~SetGlobal("CoranAN0450","AN0450",3)~ REPLY @1 + CoranAN0450Talk1
+IF ~~ THEN DO ~SetGlobal("CoranAN0450","AN0450",3)~ REPLY @2 + CoranAN0450Talk1
+IF ~~ THEN DO ~SetGlobal("CoranAN0450","AN0450",3)~ REPLY @3 + CoranAN0450Talk1
+END
+
+IF ~~ THEN BEGIN CoranAN0450Talk1
+  SAY @4
+=@5 
+=@6 
+=@7 
+IF ~~ THEN REPLY @8 + CoranAN0450Talk2
+IF ~~ THEN REPLY @9 + CoranAN0450Talk2
+IF ~~ THEN REPLY @10 + CoranAN0450Talk2
+IF ~~ THEN REPLY @11 + CoranAN0450Talk2
+END
+
+IF ~~ THEN BEGIN CoranAN0450Talk2
+  SAY @12
+IF ~~ THEN EXIT
+END
+
+END
